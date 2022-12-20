c4cb79cec9c7509183a31161ebdbd88d364761e1
Sharon Nderi <nderisharonw@gmail.com>
2022-12-20 11:20:01 +0300

    Changed code to use shorthand with rest parameters

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,5 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+export default function getBudgetObject(...allArgs) {
+  const budget = { income: allArgs[0], gdp: allArgs[1], capita: allArgs[2] };
 
   return budget;
 }
